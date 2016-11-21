29d525fa801a8d1f9e9d20885d3a134fa76d46c6
Alex <bloodhound1982@gmail.com>
2016-11-22 00:17:44 +0200

    stabil version of slider

--- src/js/TrueSlider.js
+++ src/js/TrueSlider.js
@@ -1,11 +1,19 @@
 /**
  * Created by alex on 20.11.16.
+ *
+ * @options = {} - collection of all settings
+ * @autoplay - integer (seconds)
+ * @position - integer (0...items.length)
+ *
  */
 function TrueSlider(options) {
+    var self = this;
     var slider = document.querySelector("[data-slider='slider']");
     var items = slider.querySelectorAll(".slider_item");
+    var controls = document.querySelector(".controls");
     var itemsLength = items.length;
     var position = options.position || 0;
+    var autoplay = options.autoplay;
 
     //add .show or .hidden to items
     items.forEach(function(element, index, arr) {
@@ -24,15 +32,57 @@ function TrueSlider(options) {
         slider.style.height = height + 'px';
     };
 
+    //function for creating prev-button
+    var prevConstructor = function() {
+        var prev = document.createElement('div');
+        prev.classList.add('prev');
+        prev.addEventListener('click', self.prev, true);
+        controls.appendChild(prev);
+    };
+
+    //function for creating next-button
+    var nextConstructor = function() {
+        var next = document.createElement('div');
+        next.classList.add('next');
+        next.addEventListener('click', self.next, true);
+        controls.appendChild(next);
+    };
+
+    //function for generate dots-buttons
+    var dotsConstructor = function() {
+        var pagination = document.createElement('div');
+        pagination.classList.add('.pagination');
+        for(var i = 1; i <= itemsLength; i++) {
+            var item = document.createElement('div');
+            item.innerHTML = i;
+            item.classList.add('pag_item');
+            pagination.appendChild(item);
+        }
+        pagination.children.forEach = [].forEach;
+        pagination.children.forEach(function(element, index, arr) {
+            element.addEventListener('click', function() {
+                return move(index);
+            }, true);
+        });
+        controls.appendChild(pagination);
+    };
+
+    //start (or not) slide-show
+    var startAutoplay = function() {
+        if (autoplay) {
+            setInterval(self.next, autoplay * 1000);
+        }
+    };
+
     //function with logic for moving items
-    function move(direction) {
+    var move = function(direction) {
         var destination = direction;
         if(direction === itemsLength) destination = 0;
         if(direction === -1) destination = itemsLength - 1;
         var dest = items[destination].classList;
         var current = items[position].classList;
 
-        // if (destination === position) return;
+        if (destination === position) return;
         if (position < direction) {
             dest.add('show');
             dest.add('right');
@@ -52,21 +102,32 @@ function TrueSlider(options) {
 
             position = destination;
         }
-    }
+    };
 
-    this.next = function () {
+    self.next = function () {
         move(position + 1);
-    }
+    };
 
-    this.prev = function () {
+    self.prev = function () {
         move(position - 1);
-    }
+    };
+
+    self.jumpTo = function (destination) {
+        if (destination !== position) {
+            move(destination);
+        }
+    };
 
 
     // this function wait 0.4s during item's content is loading
-    setTimeout(sliderHeight, 450);
+    setTimeout(sliderHeight, 500);
+
+    prevConstructor();
+    dotsConstructor();
+    nextConstructor();
+
+    startAutoplay();
 
     //doing slider adaptive
     window.addEventListener('resize', sliderHeight, true);
-
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,4 @@
 "use strict";
 (function () {
-    /*document.addEventListener('load', function() {
-        var slider = new TrueSlider({position: 2});
-    }, true);*/
-
-    var slider = new TrueSlider({position: 2});
-
-    document.querySelector('.next').addEventListener('click', slider.next, true);
-    document.querySelector('.prev').addEventListener('click', slider.prev, true);
-
-
-})()
+    new TrueSlider({position: 2});
+})();
