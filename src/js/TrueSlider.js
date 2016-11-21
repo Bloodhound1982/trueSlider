da88380f1125c8f9c473408f524726f377c1607a
Alex <bloodhound1982@gmail.com>
2016-11-21 22:15:33 +0200

    work method move() is complete

--- src/js/TrueSlider.js
+++ src/js/TrueSlider.js
@@ -4,36 +4,69 @@
 function TrueSlider(options) {
     var slider = document.querySelector("[data-slider='slider']");
     var items = slider.querySelectorAll(".slider_item");
+    var itemsLength = items.length;
     var position = options.position || 0;
 
+    //add .show or .hidden to items
     items.forEach(function(element, index, arr) {
         if (index === position) {
-            slider.style.height = element.offsetHeight + 'px';
             element.classList.add('show');
             return;
         }
         element.classList.add('hidden');
     });
 
-     function move(destination) {
-        if (destination === position) return;
-        if (position < destination) {
-            var dest = items[destination].classList;
-            var current = items[position].classList;
+    //function define slider height, taking item's height
+    var sliderHeight = function() {
+        var height = Math.max(items[position].offsetHeight,
+            items[position].clientHeight,
+            items[position].scrollHeight);
+        slider.style.height = height + 'px';
+    };
 
+    //function with logic for moving items
+    function move(direction) {
+        var destination = direction;
+        if(direction === itemsLength) destination = 0;
+        if(direction === -1) destination = itemsLength - 1;
+        var dest = items[destination].classList;
+        var current = items[position].classList;
+
+        // if (destination === position) return;
+        if (position < direction) {
+            dest.add('show');
             dest.add('right');
             dest.remove('hidden');
-            dest.add('show');
-
-            current.remove('show');
+            dest.remove('right');
             current.add('hidden');
+            current.remove('show');
 
+            position = destination;
+        } else {
+            dest.add('show');
+            dest.add('left');
+            dest.remove('hidden');
+            dest.remove('left');
+            current.add('hidden');
+            current.remove('show');
 
-
-            console.log([dest, current]);
+            position = destination;
         }
     }
 
-   // move(5);
+    this.next = function () {
+        move(position + 1);
+    }
+
+    this.prev = function () {
+        move(position - 1);
+    }
+
+
+    // this function wait 0.4s during item's content is loading
+    setTimeout(sliderHeight, 450);
+
+    //doing slider adaptive
+    window.addEventListener('resize', sliderHeight, true);
 
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,13 @@
 "use strict";
 (function () {
-    document.addEventListener('load', function() {
+    /*document.addEventListener('load', function() {
         var slider = new TrueSlider({position: 2});
-    }, true);
+    }, true);*/
+
+    var slider = new TrueSlider({position: 2});
+
+    document.querySelector('.next').addEventListener('click', slider.next, true);
+    document.querySelector('.prev').addEventListener('click', slider.prev, true);
 
 
 })()
